3655e35174fa798c64d5330e7ce66730fdac2d84
Alticha <27830005+Alticha@users.noreply.github.com>
2019-02-03 00:47:10 +0000

    better !16K check

--- voucher_swap/app/ViewController.m
+++ voucher_swap/app/ViewController.m
@@ -10,7 +10,6 @@
 #import "kernel_slide.h"
 #import "voucher_swap.h"
 #import "kernel_memory.h"
-#import <mach/mach.h>
 #include "post.h"
 #include <sys/utsname.h>
 
@@ -21,11 +20,31 @@
 @implementation ViewController
 
 - (bool)voucher_swap {
-    vm_size_t size = 0;
-    host_page_size(mach_host_self(), &size);
-    if (size < 16000) {
-        printf("non-16K devices are not currently supported.\n");
+#define CHECK(a, b) if (a < b) { printf("non-16k devices are unsupported.\n"); return false; }
+    if ([[UIDevice currentDevice].model isEqualToString:@"iPod touch"]) {
         return false;
+    }
+    struct utsname u;
+    uname(&u);
+    char read[257];
+    int ii = 0;
+    for (int i = 0; i < 256; i++) {
+        char chr = u.machine[i];
+        long num = chr - '0';
+        if (num == -4 || num == 0) {
+            break;
+        }
+        if (num >= 0 && num <= 9) {
+            read[ii] = chr;
+            ii++;
+        }
+    }
+    read[ii + 1] = 0;
+    int digits = atoi(read);
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+        CHECK(digits, 6);
+    } else if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        CHECK(digits, 8);
     }
     voucher_swap();
     if (!MACH_PORT_VALID(kernel_task_port)) {
@@ -33,6 +52,7 @@
         return false;
     }
     return true;
+#undef CHECK
 }
 
 - (void)failure {
@@ -41,6 +61,18 @@
 }
 
 - (IBAction)go:(id)sender {
+    /*
+     If you're running this in a method like viewDidLoad you only need the following:
+     --------------------------------------------------------------------------------
+     Post *post = [[Post alloc] init];
+     bool success = [self voucher_swap];
+     if (success) {
+        sleep(1);
+        [post go];
+     } else {
+        assert(false);
+     }
+     */
     Post *post = [[Post alloc] init];
     static int progress = 0;
     if (progress == 2) {
@@ -53,11 +85,11 @@
     progress++;
     bool success = [self voucher_swap];
     if (success) {
-	sleep(1);
+        sleep(1);
         [post go];
         [sender setTitle:@"respring" forState:UIControlStateNormal];
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"success" message:[NSString stringWithFormat:@"tfp0: %i\nkernel base: 0x%llx\nuid: %i\nunsandboxed: true", kernel_task_port, kernel_slide + 0xFFFFFFF007004000, getuid()] preferredStyle:UIAlertControllerStyleAlert];
-	[alert addAction:[UIAlertAction actionWithTitle:@"done" style:UIAlertActionStyleCancel handler:nil]];
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"success" message:[NSString stringWithFormat:@"tfp0: %i\nkernel base: 0x%llx\nuid: %i\ngid: %i\nunsandboxed: %i", kernel_task_port, kernel_slide + 0xFFFFFFF007004000, getuid(), getgid(), [post isSandboxed]] preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"done" style:UIAlertActionStyleCancel handler:nil]];
         [self presentViewController:alert animated:YES completion:nil];
     } else {
         [self failure];
