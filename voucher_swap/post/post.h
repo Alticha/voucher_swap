dd1de2e2d28062a046329b4d3712afcb839480fb
Alticha <27830005+Alticha@users.noreply.github.com>
2019-02-03 00:46:10 +0000

    Update post.h

--- voucher_swap/post/post.h
+++ voucher_swap/post/post.h
@@ -1,14 +1,24 @@
 #ifndef post_h
 #define post_h
 
-#include <stdio.h>
 #include <Foundation/Foundation.h>
 
 @interface Post : NSObject
 
 - (bool)go;
+- (bool)isRoot;
+- (bool)isSandboxed;
+- (void)setUID:(uid_t)uid;
+- (void)setGID:(gid_t)gid;
+- (void)root;
+- (void)mobile;
+- (void)unsandbox;
+- (void)sandbox;
+- (uint64_t)selfproc;
+- (uint64_t)kernproc;
+- (int)name_to_pid:(NSString *)name;
 - (void)respring;
 
 @end
 
-#endif /* post_h */
+#endif
