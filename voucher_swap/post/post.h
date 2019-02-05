0b1e25119e10a9f12cb9ecc6ded38dca852f91ce
Alticha <27830005+Alticha@users.noreply.github.com>
2019-02-05 20:01:28 +0000

    the new stuff & comments

--- voucher_swap/post/post.h
+++ voucher_swap/post/post.h
@@ -5,19 +5,29 @@
 
 @interface Post : NSObject
 
+// General
 - (bool)go;
+// Users
 - (bool)isRoot;
-- (bool)isSandboxed;
+- (bool)isMobile;
 - (void)setUID:(uid_t)uid;
 - (void)setGID:(gid_t)gid;
 - (void)root;
 - (void)mobile;
-- (void)unsandbox;
+// Sandbox
 - (void)sandbox;
+- (void)unsandbox;
+- (bool)isSandboxed;
+// Procs
+- (uint64_t)allproc;
 - (uint64_t)selfproc;
 - (uint64_t)kernproc;
-- (int)name_to_pid:(NSString *)name;
+- (uint64_t)proc_for_pid:(pid_t)pid;
+- (pid_t)pid_for_name:(NSString *)name;
 - (void)respring;
+
+// Debugging
+- (void)debug;
 
 @end
 
