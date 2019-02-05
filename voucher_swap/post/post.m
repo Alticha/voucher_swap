4f35a98d4ecaf9be3eb17b68d9d7ad8152bb82eb
Alticha <27830005+Alticha@users.noreply.github.com>
2019-02-05 20:00:47 +0000

    Update post.m

--- voucher_swap/post/post.m
+++ voucher_swap/post/post.m
@@ -7,18 +7,61 @@
 #include "parameters.h"
 
 @implementation Post
+
+// Debugging //
+
+- (void)debug {
+    bool wasMobile = [self isMobile];
+    uid_t uid = 0;
+    gid_t gid = 0;
+    if (!wasMobile) {
+        uid = getuid();
+        gid = getgid();
+        [self mobile];
+    }
+    // Breakpoint
+    if (!wasMobile) {
+        [self setUID:uid];
+        [self setGID:gid];
+    }
+}
+
+// Variables //
+
 static uint64_t SANDBOX = 0;
+static uint64_t kernel_base = 0;
+
+// General post-exploitation method //
 
 - (bool)go {
+    // Check if tfp0 is valid
     if (!MACH_PORT_VALID(kernel_task_port)) {
         return false;
     }
+    // Get root
     [self root];
+    // Unsandbox
     [self unsandbox];
+    // Kernel slide and kernel base
+    kernel_slide_init();
+    kernel_base = kernel_slide + STATIC_ADDRESS(kernel_base);
+    // Did we succeed?
     bool success = [self isRoot] && [self isSandboxed];
-    if (success) printf("Success!\nUID: 0\nUnsandboxed: true\n"); // We already know we have root and that we are unsandboxed, so we don't need to check here.
-    if (!success) printf("Failed\n");
+    if (success) printf("[POST] Success!\n");
+    if (!success) printf("[POST] Failed.\n");
+    // For debugging purposes
+    //[self debug];
     return success;
+}
+
+// Users //
+
+- (bool)isRoot {
+    return !getuid() && !getgid();
+}
+
+- (bool)isMobile {
+    return getuid() == 501 && getgid() == 501;
 }
 
 - (void)setUID:(uid_t)uid {
@@ -50,12 +93,10 @@ static uint64_t SANDBOX = 0;
     [self setGID:501];
 }
 
-- (void)unsandbox {
-    uint64_t proc = [self selfproc];
-    uint64_t ucred = kernel_read64(proc + off_p_ucred);
-    uint64_t cr_label = kernel_read64(ucred + off_ucred_cr_label);
-    if (SANDBOX == 0) SANDBOX = kernel_read64(cr_label + off_sandbox_slot);
-    kernel_write64(cr_label + off_sandbox_slot, 0);
+// Sandbox //
+
+- (bool)isSandboxed {
+    return kernel_read64(kernel_read64(kernel_read64([self selfproc] + off_p_ucred) + off_ucred_cr_label) + off_sandbox_slot) == 0;
 }
 
 - (void)sandbox {
@@ -66,12 +107,27 @@ static uint64_t SANDBOX = 0;
     SANDBOX = 0;
 }
 
-- (bool)isRoot {
-    return !getuid();
+- (void)unsandbox {
+    uint64_t proc = [self selfproc];
+    uint64_t ucred = kernel_read64(proc + off_p_ucred);
+    uint64_t cr_label = kernel_read64(ucred + off_ucred_cr_label);
+    if (SANDBOX == 0) SANDBOX = kernel_read64(cr_label + off_sandbox_slot);
+    kernel_write64(cr_label + off_sandbox_slot, 0);
 }
 
-- (bool)isSandboxed {
-    return kernel_read64(kernel_read64(kernel_read64([self selfproc] + off_p_ucred) + off_ucred_cr_label) + off_sandbox_slot) == 0;
+// Procs //
+
+- (uint64_t)allproc {
+    uint64_t proc = [self kernproc];
+    // i think the max pid value is 99998?
+    for (pid_t i = 0; i < 99999; i++) {
+        if (!kernel_read64(proc + off_p_next) /* if we can't read here, the previously read address was allproc */) {
+            return proc;
+        }
+        // not allproc - let's try this one?
+        proc = kernel_read64(proc + off_p_next);
+    }
+    return 0;
 }
 
 - (uint64_t)selfproc {
@@ -82,9 +138,23 @@ static uint64_t SANDBOX = 0;
     return kernel_read64(kernel_task + OFFSET(task, bsd_info));
 }
 
-- (int)name_to_pid:(NSString *)name {
+- (uint64_t)proc_for_pid:(pid_t)pid {
+    if (pid == getgid()) {
+        return [self selfproc];
+    } else if (pid == 0) {
+        return [self kernproc];
+    }
+    uint64_t proc = [self allproc];
+    while (proc) {
+        if (kernel_read32(proc + off_p_pid) == pid) return proc;
+        proc = kernel_read64(proc);
+    }
+    return 0;
+}
+
+- (pid_t)pid_for_name:(NSString *)name {
     static int maxArgumentSize = 0;
-    if (maxArgumentSize == 0) {
+    if (!maxArgumentSize) {
         size_t size = sizeof(maxArgumentSize);
         if (sysctl((int[]){ CTL_KERN, KERN_ARGMAX }, 2, &maxArgumentSize, &size, NULL, 0) == -1) {
             maxArgumentSize = 4096;
@@ -109,8 +179,7 @@ static uint64_t SANDBOX = 0;
             NSString *executable = [NSString stringWithCString:(buffer+sizeof(int)) encoding:NSUTF8StringEncoding];
             if ([executable isEqual:name]) {
                 return info[i].kp_proc.p_pid;
-            }
-            if ([[executable lastPathComponent] isEqual:name]) {
+            } else if ([[executable lastPathComponent] isEqual:name]) {
                 return info[i].kp_proc.p_pid;
             }
         }
@@ -121,7 +190,8 @@ static uint64_t SANDBOX = 0;
 }
 
 - (void)respring {
-    kill([self name_to_pid:@"backboardd"], SIGKILL);
+    [self unsandbox];
+    kill([self pid_for_name:@"/usr/libexec/backboardd"], SIGKILL);
 }
 
 @end
