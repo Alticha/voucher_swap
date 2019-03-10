8a2346af5a571f6d0b54b46a0ed22d21d4324162
Alticha <27830005+Alticha@users.noreply.github.com>
2019-03-10 23:57:38 +0000

    Add files via upload

--- voucher_swap/post/other/bin/compile.sh
+++ voucher_swap/post/other/bin/compile.sh
@@ -1,4 +1,4 @@
-# bash script to compile bin
+# shell script to compile bin
 if [[ "$0" != "./compile.sh" ]]; then
     echo "Argument zero is not ./compile.sh"
     exit
@@ -7,11 +7,4 @@ echo "Compiling..."
 rm bin &> /dev/null
 xcrun -sdk iphoneos clang -arch arm64 bin.c
 mv a.out bin
-echo "Done"
-echo "Tarring..."
-rm bin.tar.gz &> /dev/null
-tar -cf bin.tar.gz bin
-echo "Done"
-echo "Cleaning up..."
-rm bin &> /dev/null;
 echo "Done"
