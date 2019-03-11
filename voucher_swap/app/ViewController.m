67864ef7caecc47c7f792aa3dca87e1f5397340b
Alticha <27830005+Alticha@users.noreply.github.com>
2019-03-11 01:08:48 +0000

    Add files via upload

--- voucher_swap/app/ViewController.m
+++ voucher_swap/app/ViewController.m
@@ -2,15 +2,15 @@
 #import "kernel_slide.h"
 #import "voucher_swap.h"
 #import "kernel_memory.h"
-#include "post.h"
+#include "utilities.h"
 #include "log.h"
 #include <sys/utsname.h>
 #define colourFromHex(hex, alphaVal) [UIColor colorWithRed:((float)((hex & 0xFF0000) >> 16))/255.0 green:((float)((hex & 0xFF00) >> 8))/255.0 blue:((float)(hex & 0xFF))/255.0 alpha:alphaVal]
 #define bgDisabledColour colourFromHex(0xB8B8B8, 1.0)
-#define setBgDisabledColour setBackgroundColor:colourFromHex(0xB8B8B8, 1.0)
-#define bgEnabledColour setBackgroundColor:colourFromHex(0xFF9300, 1.0)
-#define setBgEnabledColour setBackgroundColor:colourFromHex(0xFF9300, 1.0)
-#define mainThread(code) dispatch_async(dispatch_get_main_queue(), ^{ code; }); // just to clean up the code a bit
+#define setBgDisabledColour setBackgroundColor:bgDisabledColour
+#define bgEnabledColour colourFromHex(0xFF9300, 1.0)
+#define setBgEnabledColour setBackgroundColor:bgEnabledColour
+#define mainThread(code) dispatch_async(dispatch_get_main_queue(), ^{ code; }); /* just to clean up the code a bit */
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextView *logs;
@@ -19,8 +19,14 @@
 @end
 
 @implementation ViewController
+
+// Variables //
+
 extern NSString *LOGGED;
 extern BOOL SHOULD_LOG;
+Utilities *utils;
+
+// Logging //
 
 - (void)log:(NSString *)what {
     if (!SHOULD_LOG) return;
@@ -39,6 +45,8 @@ extern BOOL SHOULD_LOG;
     mainThread([self slog:LOGGED]);
 }
 
+// voucher_swap //
+
 - (void)failure {
     ERROR("Failed");
     [_exploitBtn setTitle:@"Failed" forState:UIControlStateDisabled];
@@ -50,33 +58,39 @@ extern BOOL SHOULD_LOG;
     /*
      If you're running this in a method like viewDidLoad you only need the following:
      --------------------------------------------------------------------------------
-     Post *post = [Post alloc];
-     if ([post isUnsupported]) {
+     Utilities *u = [Utilities alloc];
+     if ([u isUnsupported]) {
         ERROR("Your device is unsupported");
         assert(false);
         return;
      }
      voucher_swap();
      if (MACH_PORT_VALID(kernel_task_port)) {
-        [post go];
+        [u post];
      } else {
         assert(false);
      }
      */
-    // Used later
-    Post *post = [Post alloc];
-    // For respringing
+    
+    // Respring
     static bool complete = false;
     if (complete) {
-        [post respring];
+        [utils respring];
         return;
     }
-    [sender setEnabled:NO];
-    [sender setTitle:@"Please Wait..." forState:UIControlStateDisabled];
+    
+    // Update the GUI
+    [_exploitBtn setEnabled:NO];
+    [_exploitBtn setTitle:@"Please Wait..." forState:UIControlStateDisabled];
+    [UIView animateWithDuration:0.5 animations:^{
+        [self->_exploitBtn setBgDisabledColour];
+    }];
+    
+    // Exploitation and post-exploitation
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         // Is the device supported?
         __block BOOL supported = true;
-        if ([post isUnsupported]) {
+        if ([utils isUnsupported]) {
             mainThread(
                        ERROR("Non-16k devices are unsupported.");
                        [sender setTitle:@"Failed" forState:UIControlStateDisabled];
@@ -96,29 +110,36 @@ extern BOOL SHOULD_LOG;
             return;
         }
         // Post exploitation
-        [post go];
+        [utils post];
         // Update the GUI
         mainThread(
                    [self->_exploitBtn setEnabled:YES];
                    [self->_exploitBtn setTitle:@"Respring" forState:UIControlStateNormal];
-                   (LOG("%s", [NSString stringWithFormat:@"[+] Kernel task port: 0x%x\n[+] Kernel base: 0x%llx\n[+] User ID: %i\n[+] Group ID: %i\n[+] Is sandboxed: %@\n[+] Done!\n", kernel_task_port, [post kernel_base], getuid(), getgid(), [post isSandboxed] ? @"Yes" : @"No"].UTF8String));
+                   [UIView animateWithDuration:0.5 animations:^{
+                       [self->_exploitBtn setBgEnabledColour];
+                   }];
+                   (LOG("%s", [NSString stringWithFormat:@"[+] Kernel task port: 0x%x\n[+] Kernel base: 0x%llx\n[+] User ID: %i\n[+] Group ID: %i\n[+] Is sandboxed: %@\n[+] Done!\n", kernel_task_port, [utils kernel_base], getuid(), getgid(), [utils isSandboxed] ? @"Yes" : @"No"].UTF8String));
                    // Become mobile ([U/G]ID: 501) so Xcode can stop the process
                    SHOULD_LOG = false;
-                   [post mobile];
+                   [utils mobile];
                    SHOULD_LOG = true;
-                   complete = true;
         );
-        complete = true;
+        complete = true; // Respring check will now return true
     });
 }
 
+// Init //
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(addLog) name:@"LoggedToString" object:nil];
+    utils = [Utilities alloc];
+    
 	INFO("Version %s", ((NSString *)[[NSBundle mainBundle].infoDictionary objectForKey:@"CFBundleShortVersionString"]).UTF8String);
     LOG("%s\n", ((NSString *)[[[NSBundle mainBundle].bundlePath componentsSeparatedByString:@"/"] objectAtIndex:5]).UTF8String);
-    Post *post = [Post alloc];
-    struct utsname u = [post uname];
-    bool isSupported = [post isSupported];
+    
+    struct utsname u = [utils uname];
+    bool isSupported = [utils isSupported];
     if (!isSupported) {
         ERROR("%s", [NSString stringWithFormat:@"%s is unsupported", u.machine].UTF8String);
         [_exploitBtn setEnabled:NO];
@@ -126,10 +147,12 @@ extern BOOL SHOULD_LOG;
         [_exploitBtn setBgDisabledColour];
         return;
     }
+    
     INFO("%s", [NSString stringWithFormat:@"%s is a supported device", u.machine].UTF8String);
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(addLog) name:@"LoggedToString" object:nil];
     INFO("Ready!");
 }
+
+// Other //
 
 - (IBAction)credits:(id)sender {
     UIAlertController *controller = [UIAlertController alertControllerWithTitle:@"Credits" message:@"(@_)bazad: Exploit\n(@)Alticha(Dev): Modifications and post-exploitation:\n(@)Pwn20wnd: Exploit reliability improvements\n(@)sbingner: ArchiveFile and trust cache injection\n(@)xerub: patchfinder64" preferredStyle:UIAlertControllerStyleAlert];
